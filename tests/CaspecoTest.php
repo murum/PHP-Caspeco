abf56cb03d991be8c9c14e531a77c0904496e0f9
Vincent Klaiber <vincentklaiber@gmail.com>
2015-10-02 16:48:31 +0200

    Update license

--- tests/CaspecoTest.php
+++ tests/CaspecoTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Caspeco.
  *
- * (c) Vincent Klaiber <hello@vinkla.com>
+ (c) Schimpanz Solutions <info@schimpanz.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
